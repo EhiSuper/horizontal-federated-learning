4fa25103ca29697e8c8c732f76a186ad6993d9be
EhiSuper <patimoantonio@gmail.com>
2022-02-11 18:13:16 +0100

    updateUser

--- src/main/java/it/unipi/dsmt/horizontalFederatedLearning/service/db/UserService.java
+++ src/main/java/it/unipi/dsmt/horizontalFederatedLearning/service/db/UserService.java
@@ -16,35 +16,35 @@ public class UserService {
         setCounterID();
     }
 
-    private void setCounterID(){
+    private void setCounterID() {
         List<String> keys = db.findKeysByPrefix("User:");
-        if(keys != null)
-            for(String key: keys){
+        if (keys != null)
+            for (String key : keys) {
                 int id = Integer.parseInt(key.split(":")[1]);
-                if(id > counterID)
+                if (id > counterID)
                     counterID = id;
             }
     }
 
     public void register(User user) throws RegistrationException {
         HashMap<String, String> map = new HashMap<>();
-        if(findUserByUsername(user.getUsername()) != null)
+        if (findUserByUsername(user.getUsername()) != null)
             throw new RegistrationException("Username already taken");
         String prefixKey = "User:" + ++counterID + ":";
-        map.put(prefixKey+"username", user.getUsername());
-        map.put(prefixKey+"firstName", user.getFirstName());
-        map.put(prefixKey+"lastName", user.getLastName());
-        map.put(prefixKey+"password", user.getPassword());
+        map.put(prefixKey + "username", user.getUsername());
+        map.put(prefixKey + "firstName", user.getFirstName());
+        map.put(prefixKey + "lastName", user.getLastName());
+        map.put(prefixKey + "password", user.getPassword());
         db.putBatchValues(map);
     }
 
     public User login(String username, String password) throws LoginException {
         List<String> keys = db.findKeysByPrefix("User:");
-        for(String key: keys){
-            if(key.endsWith("username") && db.getValue(key).equals(username)){
+        for (String key : keys) {
+            if (key.endsWith("username") && db.getValue(key).equals(username)) {
                 String id = key.split(":")[1];
                 String correctPassword = db.getValue("User:" + id + ":password");
-                if(password.equals(correctPassword))
+                if (password.equals(correctPassword))
                     return findUserByUsername(username);
                 else throw new LoginException("The password is not correct");
             }
@@ -52,10 +52,10 @@ public class UserService {
         throw new LoginException("Username not present in the database");
     }
 
-    public User findUserByUsername(String username){
+    public User findUserByUsername(String username) {
         List<String> keys = db.findKeysByPrefix("User:");
-        for(String key: keys){
-            if(key.endsWith("username") && db.getValue(key).equals(username)){
+        for (String key : keys) {
+            if (key.endsWith("username") && db.getValue(key).equals(username)) {
                 int id = Integer.parseInt(key.split(":")[1]);
                 String password = db.getValue("User:" + id + ":password");
                 String firstName = db.getValue("User:" + id + ":firstName");
@@ -66,14 +66,25 @@ public class UserService {
         return null;
     }
 
-    public User findUserById(int id){
-        List<String> keys = db.findKeysByPrefix("User:"+id);
-        if(keys.size() == 0)
+    public User findUserById(int id) {
+        List<String> keys = db.findKeysByPrefix("User:" + id);
+        if (keys.size() == 0)
             return null;
         String username = db.getValue("User:" + id + ":username");
         String password = db.getValue("User:" + id + ":password");
         String firstName = db.getValue("User:" + id + ":firstName");
         String lastName = db.getValue("User:" + id + ":lastName");
         return new User(id, firstName, lastName, username, password);
+    }
+
+    public void updateUser(User user) {
+        db.deleteValue("User:" + user.getId() + ":username");
+        db.putValue("User:" + user.getId() + ":username", user.getUsername());
+        db.deleteValue("User:" + user.getId() + ":password");
+        db.putValue("User:" + user.getId() + ":password", user.getPassword());
+        db.deleteValue("User:" + user.getId() + ":firstName");
+        db.putValue("User:" + user.getId() + ":firstName", user.getFirstName());
+        db.deleteValue("User:" + user.getId() + ":lastName");
+        db.putValue("User:" + user.getId() + ":lastName", user.getLastName());
     }
 }
